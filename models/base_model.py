6ed9a6783644cca6471609f30ad88c280a1d61e1
shane <6874@holbertonstudents.com>
2023-10-31 21:28:49 +0100

    updating class BaseModel by creation of instance from dictionnary (task4)

--- models/base_model.py
+++ models/base_model.py
@@ -18,15 +18,23 @@ class BaseModel:
     updating the last modification time, and generating unique IDs.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the class.
         Assigns a unique ID and creation timestamp.
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    if key != '__class__':
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """

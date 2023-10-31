6eceebcaad608c3b3ea372a1b6742a2c76cd9613
shane <6874@holbertonstudents.com>
2023-10-31 14:24:02 +0100

    adding correction to the basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
+        self.created_at = str(datetime.datetime.now())
         self.updated_at = self.created_at
 
     def __str__(self):
@@ -25,7 +25,20 @@ class BaseModel:
         return string
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = str(datetime.datetime.now())
 
     def to_dict(self):
-        return self.__dict__
+        """
+        This method returns the dictionary
+        representation of an instance
+        """
+
+        dico = {}
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                dico[key] = value.isoformat()
+            else:
+                dico[key] = value
+
+        dico['__class__'] = self.__class__.__name__
+        return dico

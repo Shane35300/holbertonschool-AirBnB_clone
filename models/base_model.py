97412261633498dd648f6c4d63b6bc5ae4b1f2e5
shane <6874@holbertonstudents.com>
2023-10-31 14:46:33 +0100

    last modification of the class BaseModel and its attribut methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """
-Ce script définit une classe appelée "Base"
-qui sert de base pour d'autres classes.
-La classe "Base" gère un compteur d'instances et attribue
-un identifiant unique à chaque instance créée.
+This script defines a class called "Base"
+that serves as a base for other classes.
+The "Base" class manages an instance counter and assigns
+a unique identifier to each created instance.
 """
 
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     """
-
+    This class provides a basic structure for other classes.
+    It includes methods for converting instances to dictionaries,
+    updating the last modification time, and generating unique IDs.
     """
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.datetime.now())
+        self.created_at = str(datetime.now())
         self.updated_at = self.created_at
 
     def __str__(self):
@@ -25,7 +27,10 @@ class BaseModel:
         return string
 
     def save(self):
-        self.updated_at = str(datetime.datetime.now())
+        """
+        This method make an update of the attribute updated_at
+        """
+        self.updated_at = str(datetime.now())
 
     def to_dict(self):
         """
@@ -35,8 +40,12 @@ class BaseModel:
 
         dico = {}
         for key, value in self.__dict__.items():
-            if key == 'created_at' or key == 'updated_at':
-                dico[key] = value.isoformat()
+            if key == 'created_at':
+                created_at_datetime = datetime.fromisoformat(self.created_at)
+                dico[key] = created_at_datetime.isoformat()
+            elif key == 'updated_at':
+                created_at_datetime = datetime.fromisoformat(self.updated_at)
+                dico[key] = created_at_datetime.isoformat()
             else:
                 dico[key] = value
 

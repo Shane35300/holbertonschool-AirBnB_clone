416f753c67fbc19fd8345c56323696503eb3f692
shane <6874@holbertonstudents.com>
2023-11-02 15:28:35 +0100

    trying to find a solution for thetask 5

--- models/base_model.py
+++ models/base_model.py
@@ -29,9 +29,8 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     setattr(self, key, datetime.fromisoformat(value))
-                else:
-                    if key != '__class__':
-                        setattr(self, key, value)
+                if key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()

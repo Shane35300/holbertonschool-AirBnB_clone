9a09366f31eeec8a53e396f2b7d42d768c491e3d
shane <6874@holbertonstudents.com>
2023-11-03 14:56:29 +0100

    changing names of test_user methods

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -30,7 +30,7 @@ class TestUser(unittest.TestCase):
         self.assertTrue(hasattr(user, "first_name"))
         self.assertTrue(hasattr(user, "last_name"))
 
-    def test_str_method(self):
+    def test_str(self):
         """ Test the __str__ method
         """
         user = User()
@@ -39,7 +39,7 @@ class TestUser(unittest.TestCase):
         self.assertIn(user.__class__.__name__, user_str)
         self.assertIn(user.id, user_str)
 
-    def test_to_dict_method(self):
+    def test_to_dict(self):
         """ Test to_dict method
         """
         user = User()
@@ -50,7 +50,7 @@ class TestUser(unittest.TestCase):
         self.assertEqual(user_dict["updated_at"], user.updated_at.isoformat())
         self.assertEqual(user_dict["__class__"], "User")
 
-    def test_save_method(self):
+    def test_save(self):
         """ Test save method
         """
         user = User()

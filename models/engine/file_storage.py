49ef38110500758d1398dd506a8871f12c758284
shane <6874@holbertonstudents.com>
2023-11-03 10:31:40 +0100

    adding the resolving of task 8 User class implementation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,6 +34,15 @@ class FileStorage:
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
+        if obj.__class__.__name__ == 'User':
+            email = obj.email
+            duplicates = [k for k, v in self.__objects.items() if v.__class__.__name__ == 'User' and v.email == email]
+
+            if len(duplicates) > 1:
+                # There are duplicates, remove all but the most recently created instance
+                for duplicate in duplicates[:-1]:
+                    del self.__objects[duplicate]
+
     def save(self):
         """
         Serializes the objects to a JSON file at the specified path.
@@ -51,12 +60,16 @@ class FileStorage:
         """
 
         from models.base_model import BaseModel
+        from models.user import User
 
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as file:
                 data = json.load(file)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    self.__objects[key] = BaseModel(**value)
+                    if class_name == "BaseModel":
+                        self.__objects[key] = BaseModel(**value)
+                    if class_name == "User":
+                        self.__objects[key] = User(**value)
         except FileNotFoundError:
             pass
